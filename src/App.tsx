3343cc14993401663e58a9bfc0b456533c90dc84
ENTREPRISE\goldstein <larry.goldstein@upslide.net>
2022-03-06 00:31:37 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([
+      {id: 1, name: 'Produit 1', shortDescription: 'Short desc 1'},
+      {id: 2, name: 'Produit 2', shortDescription: 'Short desc 2'}
+    ]);
+
+    useEffect(() => {
+      fetch('http://localhost:7071/api/GetAllProducts')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    }, []);
+
+    function addProducts() {
+      const ids = products.map(product => product.id);
+      setProducts(prevState => [...prevState,
+        {id: Math.max.apply(null, ids) + 1, name: 'Produit', shortDescription: 'Short desc'}]);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{color: 'green'}}>Le Troll Savant</h1>
+      <ul>
+        {products.map(product => 
+            <li key={product.id}>{product.name} #{product.id} : "{product.shortDescription}"</li>
+            )}
+      </ul>
+      <button onClick={addProducts}>Ajouter un jeu</button>
     </div>
   );
 }
